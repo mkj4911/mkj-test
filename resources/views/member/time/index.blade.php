fb2feca98a9d49bdbeae2a4e46b4217773373f76
mkj <mkj4911@gmail.com>
2022-01-16 08:00:22 +0900

    screen_correction05

--- resources/views/member/time/index.blade.php
+++ resources/views/member/time/index.blade.php
@@ -7,17 +7,17 @@
   </x-slot>
 
   <div class="py-4">
-      <div class="mx-auto w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+      <div class="mx-auto sm:max-w-lg mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white">
-                <div class="md:flex items-end mb-12 md:mb-4"><p class="text-2xl mr-4">{{$day}}</p><output id="realtime" class="text-3xl w-20 tracking-widest"></output></div>
+                <div class="flex items-end mb-12 md:mb-4"><p class="text-2xl mr-4">{{$day}}</p><output id="realtime" class="text-3xl w-20 tracking-widest"></output></div>
                   @foreach ($itmes as $itme)
                   <div class="text-lg">{{ $itme->member->name }}さん</div>
                   <div class="attendance mt-4">
                     <table>
-                      <tr class="flex items-end px-4"><td class="w-24 text-gray-700">出勤時刻：</td><td class="text-2xl tracking-widest">{{ date('H:i:s', strtotime($itme->punchIn ?? '00:00:00')) }}</td></tr>
-                      <tr class="flex items-end px-4"><td class="w-24 text-gray-700">退勤時刻：</td><td class="text-2xl tracking-widest">{{ date('H:i:s', strtotime($itme->punchOut ?? '00:00:00')) }}</td></tr>
-                      <tr class="flex items-end px-4"><td class="w-24 text-gray-700">勤務時間：</td><td class="text-2xl tracking-widest">{{$itme->workTime}}</td></tr>
+                      <tr class="flex items-center px-4"><td class="w-24 text-gray-700">出勤時刻：</td><td class="text-2xl tracking-widest">{{ date('H:i:s', strtotime($itme->punchIn ?? '00:00:00')) }}</td><td class="ml-2 text-md text-gray-700">{{ date('Y-m-d', strtotime($itme->punchIn ?? '0000-00-00')) }}</td></tr>
+                      <tr class="flex items-center px-4"><td class="w-24 text-gray-700">退勤時刻：</td><td class="text-2xl tracking-widest">{{ date('H:i:s', strtotime($itme->punchOut ?? '00:00:00')) }}</td><td class="ml-2 text-md text-gray-700">{{ date('Y-m-d', strtotime($itme->punchOut ?? '00:00:00')) }}</td></tr>
+                      <tr class="flex items-center px-4"><td class="w-24 text-gray-700">勤務時間：</td><td class="text-2xl tracking-widest">{{$itme->workTime}}</td></tr>
                     </table>
                   </div>
                   @endforeach
